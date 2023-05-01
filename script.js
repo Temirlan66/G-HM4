326fd0ac9a71abf9581088a2a7bfb632424aa9df
Temirlan <admin@Temirlans-MacBook-Pro.local>
2023-05-02 00:21:42 +0600

    hm-7

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
-const request = new XMLHttpRequest();
-request.open("GET", "peopls.json");
-request.setRequestHeader("Content-type", "application/json");
-request.send();
+const request = async () => {
+  try {
+    const response = await fetch("peopls.json");
+    const data = await response.json();
+    data.map((item) => {
+      const liList = document.createElement("li");
+      liList.innerHTML = `
+            <li>Name:${item.name}</li>
+            <li>Age:${item.age}</li>
+            `;
+      document.getElementById("ul-list").append(liList);
+    });
+  } catch (error) {
+    console.log("ERROR");
+  }
+};
 
-request.addEventListener("load", () => {
-  const response = JSON.parse(request.response);
-  response.map((item) => {
-    const liList = document.createElement("li");
-    liList.innerHTML = `
-        <li>Name:${item.name}</li>
-        <li>Age:${item.age}</li>
-        `;
-    document.getElementById("ul-list").append(liList);
-  });
-});
+request();
